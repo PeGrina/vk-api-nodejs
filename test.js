a21ffabdcceef76affd543cf93624263539c614c
PeGrina <46083846+PeGrina@users.noreply.github.com>
2020-05-20 14:08:48 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 'use strict';
-let token = "0493e38904d32274867fb68957e5e3bc8a90914a9fc563ae8303ba7a7c740e19a3f5a5cc92db6809b8c02";
+let token = proccess.env.TOKEN;
 let VK = require("./index.js");
 let lp = require("./Methods/Longpoll")
 const vk = new VK(token);
